038358e82c318059caf77ccca17e2f83f05f013b
LyalinDima <lyalindim@gmail.com>
2022-05-24 20:11:02 +0500

    Add try catch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,27 +6,32 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+    public static void main(String[] args) {
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)
                         .setSocketTimeout(30000)
                         .setRedirectsEnabled(false)
                         .build())
                 .build();
-        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
-        CloseableHttpResponse response = httpClient.execute(request);
-        String body = new String(response.getEntity().getContent().readAllBytes());
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        Type listType = new TypeToken<List<CatFact>>() {
-        }.getType();
-        List<CatFact> catFactList = gson.fromJson(body, listType);
-        catFactList.stream().filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0).forEach(System.out::println);
+        ) {
+            HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+            CloseableHttpResponse response = httpClient.execute(request);
+            String body = new String(response.getEntity().getContent().readAllBytes());
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            Type listType = new TypeToken<List<CatFact>>() {
+            }.getType();
+            List<CatFact> catFactList = gson.fromJson(body, listType);
+            catFactList.stream().filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
